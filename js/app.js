e0ff2437198eccdcb3824e11c868c95067a4995c
micarsan <miguelangel.s@260606@cesurformacion.com>
2022-10-06 03:05:34 +0200

    Añadida librería typed

--- js/app.js
+++ js/app.js
@@ -3,42 +3,44 @@
  */
 
 let set_background_body_30segundos_timmer;
-let list_add_elements_timmer = [4,5,6];
+let list_add_elements_timmer = [];
 let list_add_elements_typed = [];
+let ejecutar_todo_funciones = [];
+
 
 function background_body_aguamarina() {
     console.log('- Enunciado: Cambio de color de fondo de la página a tono azul aguamarina.');
-    document.body.style.backgroundColor="#9FD5D1";
+    document.body.style.backgroundColor = "#9FD5D1";
 }
 
 function font_body_arial() {
     console.log('- Enunciado: La fuente de la letra del body deberá ser Arial.');
-    document.body.style.fontFamily="Arial";
+    document.body.style.fontFamily = "Arial";
 }
 
 function color_body_red() {
     console.log('- Enunciado: Color rojo de las letras en el body.');
-    document.body.style.color="red";
+    document.body.style.color = "red";
 }
 
 function border_body_3px() {
     console.log('- Enunciado: Los bordes deben de tener 3px solid con color rojo.');
-    document.body.style.border="3px solid red";
+    document.body.style.border = "3px solid red";
 }
 
 function background_body_30segundos() {
     let log = '- Enunciado: Una función qué haga cambiar el fondo de color cada 30 segundos. (Pista setInterval).';
-    
+
     //comprobamos si ya hay un timmer y si es así lo cancelamos
-    if( set_background_body_30segundos_timmer ) {
+    if (set_background_body_30segundos_timmer) {
         log += '\n- Ya hay un timmer. Cancelándolo.';
         clearInterval(set_background_body_30segundos_timmer);
     }
-    
+
     log += '\n- Lanzamos el setInterval cada 30 segundos';
     set_background_body_30segundos_timmer = setInterval(set_background_body_30segundos, 30000);
-    
-    console.log( log + '\n- Lo ejecutamos la primera vez (para ver que cambia).');
+
+    console.log(log + '\n- Lo ejecutamos la primera vez (para ver que cambia).');
     set_background_body_30segundos();
 }
 function set_background_body_30segundos() {
@@ -50,8 +52,8 @@ function set_background_body_30segundos() {
         color += digitos[Math.floor(Math.random() * 16)];
     }
 
-    document.body.style.backgroundColor=color;
-    console.log( '- setInterval: Cambio color fondo a: ' + color );
+    document.body.style.backgroundColor = color;
+    console.log('- setInterval: Cambio color fondo a: ' + color);
 }
 
 
@@ -59,40 +61,39 @@ function set_background_body_30segundos() {
  * Estructuras de nodos
  */
 
- function body_firstChild_color() {
-    let log = '- Enunciado: Utiliza el primer elemento del body y cambia el color de la letra a azul.';
-    document.body.firstElementChild.style.color="blue";
-    console.log( log + '\n' + document.body.firstElementChild.innerHTML);
+function body_firstChild_color() {
+    console.log('- Enunciado: Utiliza el primer elemento del body y cambia el color de la letra a azul.');
+    document.body.firstElementChild.style.color = "blue";
 }
 
 function list_iterate() {
     console.log('- Enunciado: Entrar en el elemento lista y recorrer los objetos mostrando cada elemento iterando.');
-    
+
     // Seleccionamos todos los elementos de la lista ordenada
     let li = document.querySelectorAll('ol > li');
-    
+
     //establecemos un tiempo para disparar los eventos
     let time = 0;
-    
+
     // Recorremos los li
-    for( let item of li ) {
+    for (let item of li) {
 
         // ponemos el fondo gris
         setTimeout(() => {
-            console.log('\n- Elemento: ' + item.innerHTML);
+            console.log('\n- Resaltando el elemento de la lista: ' + item.innerHTML);
             item.classList.toggle('background-grey');
         }, time);
-        
+
         //sumamos medio segundo
-        time += 500;
+        time += 350;
 
         // Quitamos el fondo gris
         setTimeout(() => {
             item.classList.toggle('background-grey');
         }, time);
-        
+
         //sumamos medio segundo
-        time += 500;
+        time += 350;
     }
 }
 
@@ -101,61 +102,125 @@ function list_add_elements() {
 
     // Seleccionamos la lista ordenada
     let ol = document.querySelectorAll('ol')[0];
-    
+
     //establecemos un tiempo para disparar los eventos
     let time = 0;
-    
-    // Añadimos los elementos
-    let elementos = [4,5,6];
-    for( let item of elementos ) {
 
-        if( list_add_elements_typed[item] | list_add_elements_timmer[item] ) {
-            console.log('\n- Ya está creados el elemento object '+item);
+    // Añadimos los elementos
+    let elementos = [4, 5, 6];
+    for (let item of elementos) {
+
+        if (list_add_elements_typed[item] | list_add_elements_timmer[item]) {
+            console.log('\n- Ya está creado el elemento object ' + item);
         } else {
             list_add_elements_timmer[item] = setTimeout(() => {
-                console.log('\n- object ' + item);
+                console.log('\n-Añadiendo a la lista: object ' + item);
                 ol.innerHTML += '<li id="object_' + item + '"></li>';
-                list_add_elements_typed[item] = new Typed('#object_'+item, {
-                    strings: ['object '+item],
+                list_add_elements_typed[item] = new Typed('#object_' + item, {
+                    strings: ['object ' + item],
                     typeSpeed: 30,
                 });
             }, time);
-    
+
             //sumamos medio segundo
             time += 500;
         }
 
     }
 
-    
+
 }
 
 function table_title_random() {
     console.log('- Enunciado: Seleccionando el Id de la tabla, iterar y cambiar los campos de los nombres por un número random de 0 a 100.');
-    
+
+    // Cogemos la tabla por id
+    let tabla = document.getElementById('tabla');
+    // Cogemos todas las celdas de la segunda fila
+    let celdas = tabla.getElementsByTagName('tr')[1].querySelectorAll('td');
+    let time = 0;
+
+    celdas.forEach((celda, index) => {
+        // Generamos un aleatorio entre 0 y 100
+        let random = Math.floor(Math.random() * 101);
+
+        console.log('- Celda a cambiar (' + index + '): ' + celda.innerHTML
+            + '\n- Nuevo valor: ' + random);
+
+        setTimeout(() => {
+            celda.innerHTML = '';
+            let typed = new Typed(celda, {
+                strings: [String(random)],
+                typeSpeed: 30,
+            });
+        }, time);
+
+
+        //sumamos medio segundo
+        time += 150;
+    });
 }
 
 function querySelector_link_bgGreen() {
     console.log('Enunciado: Utilizar QuerySelector para elegir la clase de los enlaces y cambiar el fondo a verde.');
-    
+
+    let enlaces = document.querySelectorAll('.enlaces > a');
+
+    for (let enlace of enlaces) {
+        enlace.style.backgroundColor = "green";
+    }
+
 }
 
+
+function ejecutar_todo() {
+
+    // Cogemos todos los elementos del panel
+    let funciones = document.querySelectorAll('.panel.derecha li');
+
+    let time = 0;
+
+    // Recorremos las funciones
+    funciones.forEach((item, index) => {
+        // Excluimos la primera opción (es esta misma)
+        if (index > 0) {
+            
+            let funcion = item.getAttribute('onclick');
+            
+            // Quitamos los 2 últimos caracteres
+            funcion = funcion.substring(0, funcion.length - 2);
+
+            setTimeout(function a(){
+                item.classList.add('active');
+                window[funcion]();
+            }, time);
+    
+            setTimeout(function a(){
+                item.classList.remove('active');
+            }, time+950);
+        
+            //sumamos medio segundo
+            time += 1000;
+
+        }
+    });
+
+
+}
 
 /**
  * Funcionalidad
  */
 
 // Mostrar / ocultar los enunciados
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
     // Recorremos todos los paneles
     let panels = document.querySelectorAll(".panel");
-    for ( let item of panels ) {
+    for (let item of panels) {
 
         // Asignamos evento click a los legend
-        item.querySelector(".panel_ocultar").addEventListener("click", function() {
-            
-            console.log( 'click on: ' + item.className );
+        item.querySelector(".panel_ocultar").addEventListener("click", function () {
 
             // Toggle class
             item.classList.toggle('panel_hidden');
@@ -169,13 +234,12 @@ window.addEventListener("load", function() {
 });
 
 function reajustar_paneles() {
-   
+
     let panel_derecha = document.querySelectorAll(".derecha")[0];
     let panel_abajo = document.querySelectorAll(".abajo")[0];
-    //console.log( panel_abajo.info );
-    
+
     // Panel lateral
-    if( panel_derecha.classList.contains('panel_hidden') ) {
+    if (panel_derecha.classList.contains('panel_hidden')) {
         panel_abajo.style.right = "0px";
         document.body.style.marginRight = "26px";
     } else {
@@ -184,7 +248,7 @@ function reajustar_paneles() {
     }
 
     // Panel consola
-    if( panel_abajo.classList.contains('panel_hidden') ) {
+    if (panel_abajo.classList.contains('panel_hidden')) {
         document.body.style.marginBottom = "18px";
     } else {
         document.body.style.marginBottom = "";
@@ -194,33 +258,31 @@ function reajustar_paneles() {
 
 
 // Actualiamos la consola
-if (typeof console  != "undefined") 
+if (typeof console != "undefined")
     if (typeof console.log != 'undefined')
         console.mytemp = console.log;
     else
-        console.mytemp = function() {};
+        console.mytemp = function () { };
 
-console.log = function(message) {
+console.log = function (message) {
     console.mytemp(message);
 
     let scroll = document.querySelector('#console_log');
-    
+
     // Insertamos la hora
     let date = new Date;
-//    scroll.innerHTML += '<span class="date">'
-//        date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '</span>'
 
     // Insertamos el log
-    scroll.innerHTML +=  '<li><span class="date">'
+    scroll.innerHTML += '<li><span class="date">'
         + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '</span>'
         + '<pre>' + message + '</pre></li>';
-    
+
     //hacemos scrool hasta el final siempre
     scroll.scroll({
-        top:scroll.scrollHeight,
+        top: scroll.scrollHeight,
         behavior: 'smooth'
     });
 
-    
+
 };
-console.error = console.debug = console.info =  console.log
+console.error = console.debug = console.info = console.log
